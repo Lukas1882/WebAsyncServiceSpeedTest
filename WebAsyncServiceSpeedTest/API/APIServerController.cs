90e1571f416a55ba610658293e06f7d07211bbee
Lukas <yl1882@nyu.edu>
2017-04-30 23:49:05 -0400

    add html and api

---
+++ WebAsyncServiceSpeedTest/API/APIServerController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebAsyncServiceSpeedTest.API
+{
+    [Route("api/[controller]")]
+    public class APIServerController : Controller
+    {
+        [HttpGet]
+        public JsonResult GetAPI1()
+        {
+            Thread.Sleep(1000);
+            var record = new { id = 1 };
+            return Json(record);
+        }
+
+        [HttpGet]
+        public JsonResult GetAPI2()
+        {
+            Thread.Sleep(2000);
+            var record = new { id = 2 };
+            return Json(record);
+        }
+    }
+}
--- WebAsyncServiceSpeedTest/Controllers/HomeController.cs
+++ WebAsyncServiceSpeedTest/Controllers/HomeController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading;
+using WebAsyncServiceSpeedTest.API;
 
 namespace WebAsyncServiceSpeedTest.Controllers
 {
     public class HomeController : Controller
     {
+
         public IActionResult Index()
         {
             return View();
